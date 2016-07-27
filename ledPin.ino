4f7761cdcea95d98cf1a3cf86909bba65176db90
Vaibhav Sahu <sahu.vaibhav@gmail.com>
2016-07-27 10:13:13 +0530

    Add files via upload

---
+++ ledPin.ino
@@ -0,0 +1,22 @@
+int incomingByte = 0;
+int ledPin =13;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (Serial.available() >0) {
+  //incomingByte = Serial.read();
+  Serial.println(incomingByte, DEC);
+  if (incomingByte == 49){
+    digitalWrite(ledPin,HIGH);
+  } else if (incomingByte == 48){
+    digitalWrite(ledPin,LOW);
+  }
+  }
+  }
+
